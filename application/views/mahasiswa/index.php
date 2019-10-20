b4da2520a680f6e5271d33d1b6f8308d379b863e
Muhammad Aulia Asshiddiqie <m.aulia08@mail.unpas.ac.id>
2019-10-20 10:47:57 +0700

    membuat index mahasiswa

--- application/views/mahasiswa/index.php
+++ application/views/mahasiswa/index.php
@@ -1,38 +1,45 @@
-    <div class="container">
-        <div class="flash-data" data-flashdata="<?php $this->session-> ?>"></div>
-        <div class="row">
-            <div class="col mb-3">
+<div class="container">
+    <div class="flash-data">
+        <div class="row mt-3">
+            <div class="col-md-6">
+                <a href="<?= base_url();?>mahasiswa/tambah" class="btn btn-primary">Tambah Data Mahasiswa</a>
+            </div>
+        </div>
+
+        <div class="row mt-3">
+            <div class="col-md-6">
+                <form action="" method="post">
+                    <div class="input-group">
+                        <input type="text" class="form-control" placeholder="Cari Data Mahasiswa" name="keyword">
+                        <div class="input-group-eppend">
+                            <button class="btn btn-primary" type="submit">Cari</button>
+                        </div>
+                    </div>
+                </form>
+            </div>
+        </div>
+
+        <div class="row mt-3">
+            <div class="col-md-6">
                 <h3>Daftar Mahasiswa</h3>
-                <a href="<?= base_url('mahasiswa/tambah'); ?>" class="btn btn-primary mb-2">Tambah Data</a>
-                <table class="table">
-                    <thead>
-                        <tr>
-                            <th scope="col">#</th>
-                            <th scope="col">Nama</th>
-                            <th scope="col">NRP</th>
-                            <th scope="col">Email</th>
-                            <th scope="col">Jurusan</th>
-                            <th scope="col">aksi</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <?php 
-                        $i = 1;
-                        foreach ($mahasiswa as $mhs) : ?>
-                            <tr>
-                                <th scope="row"><?= $i++;?></th>
-                                <td><?= $mhs['nama']?></td>
-                                <td><?= $mhs['nrp']?></td>
-                                <td><?= $mhs['email']?></td>
-                                <td><?= $mhs['jurusan']?></td>
-                                <td>
-                                    <a href="" class="badge badge-primary">Ubah</a>
-                                    <a href="" class="badge badge-danger">Hapus</a>
-                                </td>
-                            </tr>
-                        <?php endforeach; ?>
-                    </tbody>
-                </table>
+                <?php if (empty($mahasiswa)) : ?>
+                   <div class="alert alert-danger" role="alert">
+                    Data Mahasiswa Tidak Ditemukan.
+                   </div>
+                <?php endif;?>
+                <ul class="list-group">
+                    <?php foreach($mahasiswa as $mhs) : ?>
+                        <li class="list-group-item">
+                        <?= $mhs['nama']; ?>
+                            <a href="<?= base_url();?>mahasiswa/hapus/<?= mhs['id'];?>" class="badge bagde-danger float-right tombol-hapus">hapus</a>
+                            
+                            <a href="<?= base_url();?>mahasiswa/ubah/<?= mhs['id'];?>" class="badge bagde-success float-right">ubah</a>
+                            
+                            <a href="<?= base_url();?>mahasiswa/detail/<?= mhs['id'];?>" class="badge bagde-primary float-right">detail</a>
+                        </li>
+                    <?php endforeach; ?>
+                </ul>   
             </div>
         </div>
     </div>
+</div>
--- application/views/template/header.php
+++ application/views/template/header.php
@@ -15,13 +15,13 @@
 	  <div class="collapse navbar-collapse" id="navbarSupportedContent">
 	    <ul class="navbar-nav mr-auto">
 	      <li class="nav-item active">
-	        <a class="nav-link" href="<?= base_url; ?>">Home <span class="sr-only">(current)</span></a>
+	        <a class="nav-link" href="<?= base_url(); ?>">Home <span class="sr-only">(current)</span></a>
 	      </li>
 	      <li class="nav-item">
-	        <a class="nav-link" href="<?= base_url; ?>/mahasiswa">Mahasiswa</a>
+	        <a class="nav-link" href="<?= base_url(); ?>/mahasiswa">Mahasiswa</a>
 	      </li>
 	      <li class="nav-item">
-	        <a class="nav-link" href="<?= base_url; ?>/about">About</a>
+	        <a class="nav-link" href="<?= base_url(); ?>/about">About</a>
 	      </li>
 	    </ul>
   	</div>
