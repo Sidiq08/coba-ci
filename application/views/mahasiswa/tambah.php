36eb7a1db435a5aae041dfd9cb57abea91ce911e
Muhammad Aulia Asshiddiqie <m.aulia08@mail.unpas.ac.id>
2019-10-20 11:17:24 +0700

    membuat function mahasiswa

--- application/views/mahasiswa/tambah.php
+++ application/views/mahasiswa/tambah.php
@@ -1,26 +1,15 @@
-<!doctype html>
-<html lang="en">
-  <head>
-    <!-- Required meta tags -->
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-
-    <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-
-    <title>Data Mahasiswa</title>
-  </head>
-  <body>
     <div class="container">
+
         <div class="row mt-3">
             <div class="col-md-6">
+
             <div class="card">
             <div class="card-header">
                 <h3>Form Tambah Data Mahasiswa</h3>
             </div>
+            
                 <div class="card-body">
-
-                   <form method="post" action="<?= base_url('mahasiswa/tambah');?>">
+                   <form method="post" action="">
                         <div class="form-group">
                         <?php if (validation_errors()) : ?>
                             <label for="nama">Nama</label>
